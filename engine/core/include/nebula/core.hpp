480299b98dfbce6ecdaec9902566b00be8f9a8e7
pooyasoleymani <pooyasoleymaninejad@gmail.com>
2025-12-09 10:02:46 +0330

    [ADD] add core library CMake file commands [ADD] add engine CMake file commands [ADD] add gitignore [ADD] add precompile header file

---
+++ engine/core/include/nebula/core.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "pch.hpp"
+
+namespace nebula {
+
+class Core {
+public:
+    std::string version() const;
+};
+
+} // namespace nebula
---
+++ engine/core/src/core.cpp
@@ -0,0 +1,3 @@
+#include "core.hpp"
+
+std::string nebula::Core::version() const { return std::string(); }
