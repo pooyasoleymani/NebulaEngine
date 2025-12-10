babf2736c7f09e3d182de8c76bd3e2039c30741d
pooyasoleymani <pooyasoleymaninejad@gmail.com>
2025-12-10 16:36:02 +0330

    [ADD] add catch2 submodule [ADD] add core unittest

---
+++ tests/core_tests/core_test.cpp
@@ -0,0 +1,10 @@
+#include "pch.hpp"
+#include "nebula/core.hpp"
+#include <catch2/catch_all.hpp>
+
+TEST_CASE("Core Test", "[Core]"){
+
+    nebula::Core core;
+    
+    REQUIRE( core.version() == "Nebula Core 0.1.0");
+};
