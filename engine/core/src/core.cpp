507ff5a291699fb8bf8dc5202202d4b655693ee7
pooyasoleymani <pooyasoleymaninejad@gmail.com>
2025-12-14 18:26:59 +0330

    [ADD] add plugin_api

--- engine/core/src/core.cpp
+++ engine/core/src/core.cpp
@@ -1,4 +1,4 @@
-#include "core.hpp"
+#include "nebula/core.hpp"
 
 std::string nebula::Core::version() const {
      return "Nebula Core 0.1.0";
--- engine/plugins/plugin_a/src/plugin_a.cpp
+++ engine/plugins/plugin_a/src/plugin_a.cpp
@@ -1,13 +1,27 @@
-#include <nebula/core.hpp>
+#include <nebula/plugin_api.hpp>
 #include <iostream>
 
 
 namespace nebula {
 
-    class PluginA {
+    class PluginA: public Plugin {
         public:
+            const char* name() const override {
+                return "PluginA";
+            }
+
+            void on_load() override {
+                info();
+            }
+
+        private:
+
             void info() {
                 std::cout << "Plugin A loaded!" << std::endl;
             }
     };  
 }; // namespace nebula
+
+extern "C" NUBELA_API nebula::Plugin* create_plugin() {
+    return new nebula::PluginA();
+}
